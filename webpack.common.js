6ee35474b597a91435b140d55d425d71c7bb4b2b
Rajat Verma <i.rajatverma21@gmail.com>
2019-07-08 22:34:03 +0530

    moment locales remover plugin

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -65,5 +66,6 @@ module.exports = {
       filename: 'index.html',
     }),
     new MiniCssExtractPlugin({ filename: 'app.css' }),
+    new MomentLocalesPlugin(),
   ],
 };
