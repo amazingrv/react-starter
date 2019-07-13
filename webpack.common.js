0b4d774fa91388233a20c8414f3adc7f26f44d48
Rajat Verma <i.rajatverma21@gmail.com>
2019-07-13 21:57:32 +0530

    loader config fix

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,7 @@ module.exports = {
         enforce: 'pre',
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: 'eslint-loader',
+        loader: 'eslint-loader',
         options: {
           fix: true,
         },
