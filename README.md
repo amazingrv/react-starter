b6117461853f515102895ec64552bd427a07a1c8
Rajat Verma <i.rajatverma21@gmail.com>
2019-09-25 22:10:17 +0530

    updated config and fixed bugs

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 # react-config-starter
+
 A Simple react starter kit with exposed config and hooks support.
+
 > Awesomeness doesn't come easy
 
-Supports routing, styled-components and eslint and prettier best practices config.
+Supports routing, styled-components, eslint, prettier and best practices in config OOB.
 
 Currently support config out-of-box:
-- browserlist with postcss for autoprefixer
-- babel-preset-env and Core-JS 3 Polyfills
-- styled-components and its babel-plugin
+
+- exposed configuration for full customization
+- browserlist support with postcss for autoprefixer
+- babel-preset-env, Core-JS 3 Polyfills and other most used plugins
+- styled-components and its babel-plugin preconfigured
 - webpack loaders for eslint, font and images etc.
 - friendly erros with formatted webpack output via plugins
+- lodash, momentjs and bulma added for fun
 
 ## `npm install`
+
 To install dependencies
 
 ## `npm start`
+
 To start webpack development server at port **9060**
 
 ## `npm run build`
-To do a minified production build with all optimization enabled
+
+To do a minified production build with all optimizations enabled
--- src/pages/demo/demo.jsx
+++ src/pages/demo/demo.jsx
@@ -1,85 +1,12 @@
-import React, { Component } from 'react';
-import CloseableTabs from '../../lib/components/closeable-tabs';
+import React from 'react';
+import Header from '../../lib/components/Header';
 
-export default class Demo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [
-        {
-          tab: 'List',
-          component: (
-            <div>
-              <h1>Your list</h1>
-            </div>
-          ),
-          id: 0,
-        },
-        {
-          tab: 'Item detail 1',
-          component: <div>Item details for 1</div>,
-          id: 1,
-          closeable: true,
-        },
-        {
-          tab: 'Item detail 2',
-          component: <div>Item details for 2</div>,
-          id: 2,
-          closeable: true,
-        },
-        {
-          tab: 'Item detail 3',
-          component: <div>Item details for 3</div>,
-          id: 3,
-          closeable: true,
-        },
-      ],
-      activeIndex: 0,
-    };
-  }
+const Demo = () => {
+  return (
+    <div>
+      <Header>Welcome to React Super!</Header>
+    </div>
+  );
+};
 
-  addItem = () => {
-    const id = new Date().valueOf();
-    this.setState(state => ({
-      data: state.data.concat({
-        tab: `New item ${id}`,
-        component: <div>Your new component data for {id.toString()}</div>,
-        id: id.toString(),
-        closeable: true,
-      }),
-      activeIndex: state.data.length,
-    }));
-  };
-
-  handleTabClose = (id, newIndex) => {
-    this.setState(state => ({
-      data: state.data.filter(item => item.id !== id),
-      activeIndex: newIndex,
-    }));
-  };
-
-  handleTabClick = (id, newIndex) => {
-    this.setState({
-      activeIndex: newIndex,
-    });
-  };
-
-  render() {
-    const { data, activeIndex } = this.state;
-    return (
-      <div>
-        <button type="button" onClick={this.addItem}>
-          Add item
-        </button>
-        <CloseableTabs
-          tabPanelColor="lightgray"
-          data={data}
-          onCloseTab={this.handleTabClose}
-          onTabClick={this.handleTabClick}
-          activeIndex={activeIndex}
-        />
-        <div style={{ padding: '10px' }}>{data[activeIndex].component}</div>
-      </div>
-    );
-  }
-}
+export default Demo;
